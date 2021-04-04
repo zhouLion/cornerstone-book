e0d3efe06674557b45391527581465ccb8bb37a7
zhouLion <zhouquan147369@gamil.com>
2021-04-04 16:24:06 +0800

    +1

--- cornerstone-core/.vuepress/config.js
+++ cornerstone-core/.vuepress/config.js
@@ -1,25 +1,28 @@
 module.exports = {
-    base: "/cornerstone-book/",
-    locales: {
-      // 键名是该语言所属的子路径
-      // 作为特例，默认语言可以使用 '/' 作为其路径。
-      '/': {
-        lang: 'en-US', // 将会被设置为 <html> 的 lang 属性
-        title: 'Cornerstone',
-        description: 'Cornerstone is an open source project with a goal to deliver a complete web based medical imaging platform.'
-      },
-      '/zh/': {
-        lang: '中文',
-        title: 'Cornerstone',
-        description: 'Cornerstone 是一个开源项目，致力于交付一个基于 web 端的医疗影像平台。'
-      }
+  plugins: ['@vuepress/last-updated'],
+  base: "/cornerstone-book/",
+  locales: {
+    // 键名是该语言所属的子路径
+    // 作为特例，默认语言可以使用 '/' 作为其路径。
+    '/': {
+      lang: 'en-US', // 将会被设置为 <html> 的 lang 属性
+      title: 'Cornerstone',
+      description: 'Cornerstone is an open source project with a goal to deliver a complete web based medical imaging platform.'
     },
-    themeConfig: {
-        sidebar: "auto",
-        nav: [
-            { text: 'Home', link: '' },
-            { text: 'Guide', link: 'guide/' },
-            { text: 'External', link: 'https://google.com' },
-        ]
+    '/zh/': {
+      lang: '中文',
+      title: 'Cornerstone',
+      description: 'Cornerstone 是一个开源项目，致力于交付一个基于 web 端的医疗影像平台。'
     }
+  },
+
+  themeConfig: {
+    lastUpdated: 'Last Updated',
+    sidebar: "auto",
+    nav: [
+      { text: 'Home', link: '' },
+      { text: 'Guide', link: 'guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ]
   }
+}
--- cornerstone-core/zh/README.md
+++ cornerstone-core/zh/README.md
@@ -1,16 +1,24 @@
 ---
 home: true
-heroImage: /hero.png
-heroText: Hero 标题
-tagline: Hero 副标题
-actionText: 快速上手 →
+heroImage: https://avatars.githubusercontent.com/u/34072764?s=200&v=4
+heroText: Cornerstone 核心
+tagline: web 端医学影像驱动
+actionText: 查看文档
 actionLink: /zh/SUMMARY/
 features:
-- title: 简洁至上
-  details: 该文档重点介绍 Cornerstone 核心库，它是一个轻量级的 JavaScript 库，用于在支持 HTML5 canvas 元素的现代浏览器上，展示医学影像。Cornerstone 核心库本身并不是一个完整的应用，而是一个作用于更大型、更复杂应用中的组件。
-- title: Vue驱动
-  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
-- title: 高性能
-  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
-footer: MIT Licensed | Copyright © 2018-present Evan You
+- title: 轻量/现代化
+  details: 基于 HTML5/Javascript 在 web 应用上提供可交互性的医学影像
+- title: 灵活扩展
+  details: 为开发给复杂的企业级医疗影像查看器、报告查看器提供基础
+- title: 支持多设备
+  details: 支持所有基于 HTML5 的浏览器，包括移动设备、平板和桌面电脑
+- title: 支持多格式
+  details: 可展示所有的医学影像 (如： 8 bit grayscale, 16 bit grayscale, RGB color)
+- title: 图像展示性能优越
+  details: 通过Image Loader插件设计从具有不同协议的不同系统中获取图像
+- title: 可交互性
+  details: API支持更改视口属性（例如，ww / wc，缩放，平移，反转）
+footer: 为天朝医疗做💉💉贡献
 ---
+
+
