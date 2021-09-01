d8c6b5b282e488a6f7c4ca14d5fc89e68b4954ac
zhouLion <zhouquan147369@gamil.com>
2021-09-01 13:31:07 +0800

    add cornerstoneTool v2 examples

--- cornerstone-doc/.vuepress/nav/en.js
+++ cornerstone-doc/.vuepress/nav/en.js
@@ -22,7 +22,13 @@ module.exports = (base) => ([
 			target:"_blank"
 		},
 		{
-			text: "cornerstoneTools",        
+			text: "cornerstoneTools V2",
+			link: path.resolve(base, 'cornerstone-tools-2-examples/index.html'),
+			rel: "noopener",
+			target:"_blank"
+		},
+		{
+			text: "cornerstoneTools latest",        
 			link: path.resolve(base, 'cst-examples/index.html'),
 			rel: "noopener",
 			target:"_blank"
@@ -36,78 +42,14 @@ module.exports = (base) => ([
 	{
 		text: "plugins",
 		items: [
-			// {
-			// 	text: "Image Loader",
-			// 	items: [
-			// 		{
-			// 			text: "xxx",
-			// 			link: "/",
-			// 			rel: "noopener",
-			// 			target: "_blank"
-			// 		}
-			// 	]
-			// },
 			{
 				text: "cornerstoneTools",
 				items: [
-          toolsVersionItem("latest", base),
-          toolsVersionItem("v2", base),
-          toolsVersionItem("v3", base),
+					toolsVersionItem("latest", base),
+					toolsVersionItem("v2", base),
+					toolsVersionItem("v3", base),
 				]
 			},
 		]
 	},
-	// {
-	// 	text: 'show more',
-	// 	ariaLabel: '了解更多',
-	// 	items: [
-	// 		{
-	// 			text: 'API',
-	// 			items: [
-	// 				{
-	// 					text: 'CLI',
-	// 					link: '/api/cli.html'
-	// 				},
-	// 				{
-	// 					text: 'Node',
-	// 					link: '/api/node.html'
-	// 				}
-	// 			]
-	// 		},
-	// 		{
-	// 			text: '开发指南',
-	// 			items: [
-	// 				{
-	// 					text: '本地开发',
-	// 					link: '/miscellaneous/local-development.html'
-	// 				},
-	// 				{
-	// 					text: '设计理念',
-	// 					link: '/miscellaneous/design-concepts.html'
-	// 				},
-	// 				{
-	// 					text: 'FAQ',
-	// 					link: '/faq/'
-	// 				},
-	// 				{
-	// 					text: '术语',
-	// 					link: '/miscellaneous/glossary.html'
-	// 				}
-	// 			]
-	// 		},
-	// 		{
-	// 			text: '其他',
-	// 			items: [
-	// 				{
-	// 					text: '从 0.x 迁移',
-	// 					link: '/miscellaneous/migration-guide.html'
-	// 				},
-	// 				{
-	// 					text: 'Changelog',
-	// 					link: 'https://github.com/vuejs/vuepress/blob/master/CHANGELOG.md'
-	// 				}
-	// 			]
-	// 		}
-	// 	]
-	// },
 ]);
--- cornerstone-doc/.vuepress/nav/zh.js
+++ cornerstone-doc/.vuepress/nav/zh.js
@@ -22,7 +22,13 @@ module.exports = (base) => ([
         target:"_blank"
       },
       {
-        text: "cornerstoneTools",        
+        text: "cornerstoneTools V2",
+        link: path.resolve(base, 'cornerstone-tools-2-examples/index.html'),
+        rel: "noopener",
+        target:"_blank"
+      },
+      {
+        text: "cornerstoneTools 最新",
         link: path.resolve(base, 'cst-examples/index.html'),
         rel: "noopener",
         target:"_blank"
@@ -36,17 +42,6 @@ module.exports = (base) => ([
 	{
 		text: "插件",
 		items: [
-			// {
-			// 	text: "图像加载器",
-			// 	items: [
-			// 		{
-			// 			text: "xxx",
-			// 			link: "/zh/",
-			// 			rel: "noopener",
-			// 			target: "_blank"
-			// 		}
-			// 	]
-			// },
 			{
 				text: "基石工具库",
 				items: [
@@ -57,57 +52,4 @@ module.exports = (base) => ([
 			},
 		]
 	},
-	// {
-  //   text: '了解更多',
-  //   ariaLabel: '了解更多',
-  //   items: [
-  //     {
-  //       text: 'API',
-  //       items: [
-  //         {
-  //           text: 'CLI',
-  //           link: '/zh/api/cli.html'
-  //         },
-  //         {
-  //           text: 'Node',
-  //           link: '/zh/api/node.html'
-  //         }
-  //       ]
-  //     },
-  //     {
-  //       text: '开发指南',
-  //       items: [
-  //         {
-  //           text: '本地开发',
-  //           link: '/zh/miscellaneous/local-development.html'
-  //         },
-  //         {
-  //           text: '设计理念',
-  //           link: '/zh/miscellaneous/design-concepts.html'
-  //         },
-  //         {
-  //           text: 'FAQ',
-  //           link: '/zh/faq/'
-  //         },
-  //         {
-  //           text: '术语',
-  //           link: '/zh/miscellaneous/glossary.html'
-  //         }
-  //       ]
-  //     },
-  //     {
-  //       text: '其他',
-  //       items: [
-  //         {
-  //           text: '从 0.x 迁移',
-  //           link: '/zh/miscellaneous/migration-guide.html'
-  //         },
-  //         {
-  //           text: 'Changelog',
-  //           link: 'https://github.com/vuejs/vuepress/blob/master/CHANGELOG.md'
-  //         }
-  //       ]
-  //     }
-  //   ]
-  // },
 ]);
